204e189cf04e329b4f21b6a28007a513e57477bb
Yuyoung KIM <yuykim14@gmail.com>
2025-11-10 12:00:39 +0900

    dog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import dog from './dog.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={dog} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
